37f8cd218f2a21f407f9ace69bfd42599ccc4812
ZJJeffery <359191701@qq.com>
2015-05-20 20:21:28 +0800

    first commit

---
+++ ZJPhotoBrowser/ZJPhotoBrowser/ZJPhotoBrowser.h
@@ -0,0 +1,19 @@
+//
+//  ZJPhotoBrowser.h
+//  ZJPhotoBrowser
+//
+//  Created by Jiajun Zheng on 15/5/20.
+//  Copyright (c) 2015年 hgProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZJPhotoBrowser.
+FOUNDATION_EXPORT double ZJPhotoBrowserVersionNumber;
+
+//! Project version string for ZJPhotoBrowser.
+FOUNDATION_EXPORT const unsigned char ZJPhotoBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZJPhotoBrowser/PublicHeader.h>
+
+
